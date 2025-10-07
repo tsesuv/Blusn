18a05b05f0df9df2aa6c4b3a100d49635ad471b5
tsesuv <tsesuv08@gmail.com>
2025-10-07 21:56:57 +0900

    不要になったファイルの整理とか

---
+++ src/cmd/xu/xu.c
@@ -0,0 +1,25 @@
+/* MS-DOSのDEBUG.EXEみたいな、それよりもちょっと高機能でリッチなアセンブラ */
+/* 現状名前は適当に入力してる(つまり仮名) */
+/* Version: 1.0.0 Pre-Alpha */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int version(void)
+{
+	printf \
+	( \
+	"" \
+	);
+	exit(0);
+}
+
+int help(void)
+{
+	exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
