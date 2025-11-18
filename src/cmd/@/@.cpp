204427ca542c516a765e4faab07ed5ae582330e1
tsesuv <tsesuv08@gmail.com>
2025-11-18 20:27:42 +0900

    @言語をコンパイラ言語からREPL/インタプリタ言語にしようとしてる

--- src/cmd/@/@.cpp
+++ src/cmd/@/@.cpp
@@ -3,51 +3,43 @@
 
 ////////////////////////////////////////////////////////
 
-
 int version(void);
 int help(void);
 
 ////////////////////////////////////////////////////////
 
-int version(void)
-{
-	std::cout << "    UnSynk @ Compiler" << std::endl;
-	std::cout << "------------------------" << std::endl;
-	std::cout << "Version: 1.0.0 Pre-Alpha" << std::endl;
-	std::cout << "Build  : 2025103101"      << std::endl;
-	std::cout << "------------------------" << std::endl;
-	std::cout << "enter `@ /?' you get more helps.\n" << std::endl;
-
-	return -1;
-	exit(0);
-}
-
-int help(void)
-{
-	std::cout << "    UnSynk @ Compiler" << std::endl;
-	std::cout << "------------------------" << std::endl;
-	std::cout << "Usage:"                   << std::endl;
-	std::cout << "\t@ src.@ </o:filename> </w>" << std::endl;
-	std::cout << "------------------------" << std::endl;
-	std::cout << "Options:" << std::endl;
-	std::cout << "\t/o\t:\tSpecify output file name and file path." << std::endl;
-	std::cout << "\t/w\t:\tEnable additional warnings." << std::endl;
-	std::cout << "\t/?\t:\tPrint this help mesage." << std::endl;
-
-	exit(0);
-	return -1;
-}
-
-////////////////////////////////////////////////////////
-
 int main(int argc, char **argv)
 {
-	if(argc < 2)
-	{
-		version();
-	}
+	if(argc < 2) exit(version());
 
 	return 0;
 }
 
 ////////////////////////////////////////////////////////
+
+int version(void)
+{
+	std::cout << "  UnSynk @ REPL & Interpreter" << std::endl;
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << " Version: 1.0.0 Pre-Alpha" << std::endl;
+	std::cout << " Build  : 2025103101"      << std::endl;
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << "enter `@ /?' you get more helps.\n" << std::endl;
+
+	return 0;
+}
+
+int help(void)
+{
+	std::cout << "  UnSynk @ REPL & Interpreter" << std::endl;
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << "Usage:"                   << std::endl;
+	std::cout << "\t@ src.@ </o:filename> </w>" << std::endl;
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << "\t/o\t:\tSpecify output file name and file path." << std::endl;
+	std::cout << "\t/w\t:\tEnable additional warnings." << std::endl;
+	std::cout << "\t/?\t:\tPrint this help mesage." << std::endl;
+
+	return 0;
+}
