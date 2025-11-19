dad446a8bf6e8d79f847497dbbb4f95edcde6a86
tsesuv <tsesuv08@gmail.com>
2025-11-19 10:10:19 +0900

    足りてなかった設定コマンドの追加、名前の変更

--- src/cmd/@/@.cpp
+++ src/cmd/@/@.cpp
@@ -3,8 +3,6 @@
 
 ////////////////////////////////////////////////////////
 
-bool IsREPL = true;
-
 ////////////////////////////////////////////////////////
 
 bool version(void);
@@ -14,8 +12,6 @@ bool help(void);
 
 int main(int argc, char **argv)
 {
-	if(1 < argc) IsREPL = false;
-
 	return 0;
 }
 
@@ -23,7 +19,7 @@ int main(int argc, char **argv)
 
 bool version(void)
 {
-	std::cout << "      UnSynk @ Evaluator" << std::endl;
+	std::cout << "      UnSynk @ Evalizer" << std::endl;
 	std::cout << "------------------------------" << std::endl;
 	std::cout << " Version: 1.0.0 Pre-Alpha" << std::endl;
 	std::cout << " Build  : 2025111901"      << std::endl;
@@ -38,17 +34,18 @@ bool help(void)
 	std::cout << "      UnSynk @ Evaluator" << std::endl;
 	std::cout << "------------------------------" << std::endl;
 	std::cout << "Usage:"                   << std::endl;
-	std::cout << "	@ </hidden:{true|false}> </delay:{true|false}> </src:[\\file\\path.@]" << std::endl;
+	std::cout << "	@ </autoexit:{true|false}> </hidden:{true|false}> </delay:{true|false}> </src:[\\file\\path.@]" << std::endl;
 	std::cout << "------------------------------" << std::endl;
 	std::cout << "Options:" << std::endl;
-	std::cout << "	/hidden	:	Enable output hidden flag." << std::endl;
-	std::cout << "	/delay	:	Enable delay execute flag." << std::endl;
-	std::cout << "	/src	:	Configuration source file path." << std::endl;
-	std::cout << "	/?		:	Print this help message." << std::endl;
-	std::cout << "	/help	:	Print this help message." << std::endl;
-	std::cout << "	/h		:	Print this help message." << std::endl;
-	std::cout << "	/v		:	Print software informations." << std::endl;
-	std::cout << "	/version:	Print software informations." << std::endl;
+	std::cout << "	/autoexit	:	Enable auto exit flag." << std::endl;
+	std::cout << "	/hidden		:	Enable output hidden flag." << std::endl;
+	std::cout << "	/delay		:	Enable delay execute flag." << std::endl;
+	std::cout << "	/src		:	Configuration source file path." << std::endl;
+	std::cout << "	/?			:	Print this help message." << std::endl;
+	std::cout << "	/help		:	Print this help message." << std::endl;
+	std::cout << "	/h			:	Print this help message." << std::endl;
+	std::cout << "	/v			:	Print software informations." << std::endl;
+	std::cout << "	/version	:	Print software informations." << std::endl;
 
 	return true;
 }
