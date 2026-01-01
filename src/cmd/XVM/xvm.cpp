21df4512c628eb6aaabb79ab864917dab51d8bda
tsesuv notsel <tsesuv08@gmail.com>
2026-01-02 00:27:57 +0900

    XMC ISAの更新: EPSH/EPOP命令の追加 EPSH: SPをPUSHした時と同じ動きをさせる(データは一切PUSHされない) EPOP: SPをPOPした時と同じ動きをさせる(データは一切POPされない)

--- src/cmd/XVM/xvm.cpp
+++ src/cmd/XVM/xvm.cpp
@@ -3,69 +3,83 @@
 /* Created by UnSynk, tsesuv nostel */
 
 #include "xvm.inc"
-#include "cpu.inc"
-#include "exec.inc"
-#include "inst.inc"
-#include "mem.inc"
+#include "core.inc"
+
+////////////////////////////////////////////////////////
+
+bool version(void);
+bool help(void);
+
+bool dbgprnt(void);
 
 ////////////////////////////////////////////////////////
 
 int main(int ac, char **av)
 {
 	MEM mem;
-	Inst inst;
-	std::vector<uint8_t> bytes;
-	InstInfo info;
-	Exec exe;
 	CPU cpu;
+	Inst inst;
+	InstInfo info;
+	Exec exec;
+	Core core;
 
-	mem.init(1024);
-	cpu.initMem(&mem);
-	cpu.reset();
-	inst.initTable(&mem);
-	exe.init(&cpu, &mem, &inst);
+	std::string args;
+	for(int t = 0; t < ac; t++) args += std::string(av[t]) + " ";
 
-	mem.writeByte(0, 0x00);
+	std::string ifname = "";
+	std::string ofname = "";
 
-	mem.writeByte(1, 0x01);
-	mem.writeByte(2, 0x00);
-	mem.writeByte(3, 0x00);
+	bool hasOut = false;
 
-	mem.writeByte(4, 0x0F);
+	std::regex i_patt(R"(/(?:i:|if=|infile:)([^\s/]+))");
+	std::regex o_patt(R"(/(?:o:|of=|outfile:)([^\s/]+))");
+	std::regex oth_patt1(R"(/(\?|help))");
+	std::regex oth_patt2(R"(/(v|version))");
+	std::smatch i_match;
+	std::smatch o_match;
+	std::smatch oth_match1;
+	std::smatch oth_match2;
 
-	mem.writeByte(5, 0x03);
-	mem.writeByte(6, 0x01);
-	mem.writeByte(7, 0x00);
-	mem.writeDouble(8, 0x01000000);
+	if(std::regex_search(args, i_match, i_patt)) ifname = i_match[1].str();
+	if(std::regex_search(args, o_match, o_patt)) ofname = o_match[1].str();
+	if(std::regex_search(args, oth_match1, oth_patt1)) exit(!help());
+	if(std::regex_search(args, oth_match2, oth_patt2)) exit(!version());
 
-	mem.writeDouble(12, 0x00000000);
-
-	mem.writeDouble(16, 0x02010105);
-	mem.writeDouble(20, 0x01000000);
-
-	mem.writeDouble(24, 0x00000000);
-
-	if(inst.decode(0, bytes, 10, &info)) printf("Decoded: len = %d, hasImm = %d\n", info.len, info.hasImm);
-
-	int al = 0;
-	while(cpu.getPC() < 30)
+	if(ifname.empty())
 	{
-		bytes.clear();
-		inst.decode(cpu.getPC(), bytes, 4, &info);
-		exe.run(0, info);
-		printf("Decoded: len = %d, hasImm = %d\n", info.len, info.hasImm);
-		for(int i = 0; i < bytes.size(); i++) std::cout << std::hex << (int)bytes[i] << std::dec << " ";
-		std::cout << std::endl;
-		std::cout << "PC is " << std::hex << cpu.getPC() << std::dec << " now" << std::endl;
-		std::cout << "EAX: " << std::hex << *cpu.getReg(0) << std::dec << std::endl;
-		std::cout << "EBX: " << std::hex << *cpu.getReg(1) << std::dec << std::endl;
+		std::cerr << "[ERROR] XASM: No input file specified" << std::endl;
+		return 1;
+	}
 
-		if(al % 2 == 0) cpu.setIRQ(cpu.IRQ_TIMER);
-		else cpu.clrIRQ(cpu.IRQ_TIMER);
-		al++;
+	if(!ofname.empty()) hasOut = true;
+
+	core.init(1048576);
+
+	while(core.power)
+	{
+		core.exeVM(1048576);
+
+		cpu.setIRQ(!cpu.getIRQ(cpu.FLAG_R));
 	}
 
 	mem.free();
 
 	return 0;
+}
+
+////////////////////////////////////////////////////////
+
+bool version(void)
+{
+	return true;
+}
+
+bool help(void)
+{
+	return true;
+}
+
+bool dbgprnt(void)
+{
+	return true;
 }
